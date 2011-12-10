d09a7014100c8b3ee72f27a8d9464d30b94c16f4
drew <drew@drew-desktop.(none)>
2011-12-10 04:22:22 -1000

    default server

---
+++ server.js
@@ -0,0 +1,33 @@
+http = require('http');
+fs = require('fs');
+exec = require('child_process').exec;
+sys = require('sys');
+index = fs.readFileSync(__dirname + '/index.html', 'utf8');
+index = index.replace(/ENV_DEBUG/, sys.inspect(process.env));
+index = index.replace(/https:\/\/no\.de\/smartmachines\//,
+                      "https://no.de/smartmachines/" +
+                      process.env.SMF_ZONENAME);
+index_buffer = new Buffer(index)
+
+favicon = fs.readFileSync(__dirname + '/favicon.ico');
+
+server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.writeHead(200, {'content-type': 'text/html',
+                        'content-length': index_buffer.length });
+    res.end(index_buffer);
+  } else if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'content-type': 'image/x-icon',
+                        'content-length': favicon.length });
+    res.end(favicon);
+  } else {
+    res.writeHead(400, {'content-type': 'text/plain'});
+    res.end('Not found');
+  }
+});
+
+port = process.env.PORT || 8000;
+
+server.listen(+port);
+
+console.log("Listening on port " + port);
