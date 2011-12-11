5176350a58f27993567183734d96f77477494645
Andrew <shixish@gmail.com>
2011-12-11 02:49:00 -1000

    new codebase using hbs (handlebars) and express

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-var http = require('http'),
-    fs = require('fs'),
-    //exec = require('child_process').exec,
-    sys = require('sys');
 
-var io = require('socket.io'),
-    _ = require('underscore')._,
-    backbone = require('backbone'),
-    Handlebars = require('handlebars');
+/**
+ * Module dependencies.
+ */
 
-var index_html = fs.readFileSync(__dirname + '/index.html', 'utf8'),
-    template = Handlebars.compile(index_html),
-    template_data = {'ENV_DEBUG':sys.inspect(process.env), 'HANDLEBARS':sys.inspect(Handlebars)},
-    index_buffer = new Buffer(template(template_data)),
-    favicon = fs.readFileSync(__dirname + '/favicon.ico');
+var express = require('express')
+  , routes = require('./routes')
+  
+var util = require('util');
+var hbs = require('hbs');
 
-server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    res.writeHead(200, {'content-type': 'text/html', 'content-length': index_buffer.length });
-    res.end(index_buffer);
-  } else if (req.url === '/favicon.ico') {
-    res.writeHead(200, {'content-type': 'image/x-icon', 'content-length': favicon.length });
-    res.end(favicon);
-  } else {
-    res.writeHead(400, {'content-type': 'text/plain'});
-    res.end('Not found');
-  }
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hdb');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-port = process.env.PORT || 8000;
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
 
-server.listen(+port);
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
 
-console.log("Listening on port " + port);
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
