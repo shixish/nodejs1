b58692ecd61c1d1e000c3bf0fea1239e7b21d530
Andrew <shixish@gmail.com>
2011-12-11 02:59:18 -1000

    try again

---
+++ server.js
@@ -0,0 +1,71 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+  
+var util = require('util');
+var hbs = require('hbs');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+/*app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express Handlebars Test',
+    // basic test
+    name: 'Alan',
+    hometown: "Somewhere, TX",
+    kids: [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}],
+    // path test
+    person: { "name": "Alan" }, company: {"name": "Rad, Inc." },
+    // escapee test
+    escapee: '<jail>escaped</jail>',
+    // helper test
+    posts: [{url: "/hello-world", body: "Hello World!"}],
+    // helper with string
+    posts2: [{url: "/hello-world", body: "Hello World!"}],
+    // for block helper test
+    people: [
+      {firstName: "Yehuda", lastName: "Katz"},
+      {firstName: "Carl", lastName: "Lerche"},
+      {firstName: "Alan", lastName: "Johnson"}
+    ],
+    people2: [
+      { name: { firstName: "Yehuda", lastName: "Katz" } },
+      { name: { firstName: "Carl", lastName: "Lerche" } },
+      { name: { firstName: "Alan", lastName: "Johnson" } }
+    ],
+    // for partial test
+    people3: [
+      { "name": "Alan", "id": 1 },
+      { "name": "Yehuda", "id": 2 }
+    ]
+  });
+});*/
+
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
