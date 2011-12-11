2506d4eb0ef6c6fa206ff2662c3166a417121e09
Andrew <shixish@gmail.com>
2011-12-11 02:54:13 -1000

    trying to fix server.js

--- server.js
+++ server.js
@@ -32,7 +32,40 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express Handlebars Test',
+    // basic test
+    name: 'Alan',
+    hometown: "Somewhere, TX",
+    kids: [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}],
+    // path test
+    person: { "name": "Alan" }, company: {"name": "Rad, Inc." },
+    // escapee test
+    escapee: '<jail>escaped</jail>',
+    // helper test
+    posts: [{url: "/hello-world", body: "Hello World!"}],
+    // helper with string
+    posts2: [{url: "/hello-world", body: "Hello World!"}],
+    // for block helper test
+    people: [
+      {firstName: "Yehuda", lastName: "Katz"},
+      {firstName: "Carl", lastName: "Lerche"},
+      {firstName: "Alan", lastName: "Johnson"}
+    ],
+    people2: [
+      { name: { firstName: "Yehuda", lastName: "Katz" } },
+      { name: { firstName: "Carl", lastName: "Lerche" } },
+      { name: { firstName: "Alan", lastName: "Johnson" } }
+    ],
+    // for partial test
+    people3: [
+      { "name": "Alan", "id": 1 },
+      { "name": "Yehuda", "id": 2 }
+    ]
+  });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
