cc25c943ae869648262e0162bcbfa266f5039339
Andrew <shixish@gmail.com>
2011-12-18 22:28:47 -1000

    Trying socket.io

--- server.js
+++ server.js
@@ -63,9 +63,9 @@ app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var io = sio.listen(1337);
-io.configure(function () {
+/*io.configure(function () {
   io.set('transports', ['websocket']);
-});
+});*/
 io.sockets.on('connection', function (socket) {
   //socket.emit('messege', { hello: 'world' });
   socket.send('hi');
