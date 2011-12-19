f4cebcbcea336247a7f84ca78c247be304e2c2bf
Andrew <shixish@gmail.com>
2011-12-18 21:05:21 -1000

    Trying socket.io

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
-    socket.emit('news', { hello: 'world' });
+    //socket.emit('news', { hello: 'world' });
   });
 });
